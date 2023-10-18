b9f205a369daceebaaf090ab9fd630497052acab
YuzeTT <ziv1024@163.com>
2023-10-18 17:00:04 +0800

    chore: add google analytics

--- src/components/Line.tsx
+++ src/components/Line.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export default function Line({zh, en, logo, children}:{zh: string, en:string, logo:ReactNode, children?:ReactNode}) {
   return (
-    <div className="-mx-4 px-4 bg-zinc-50 py-2 mb-2 rounded-0 sm:rounded-xl" flex='~ items-center'>
+    <div className="-mx-4 px-4 bg-zinc-50 py-2 mb-2 rounded-0 sm:rounded-xl sm:mt-2" flex='~ items-center'>
       {logo}
       <div className='flex-1'>
         <div text='xl' >{zh}</div>
