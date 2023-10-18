0f4e5ce28b6b66da6f654c046c43d7619696e198
YuzeTT <ziv1024@163.com>
2023-10-18 17:46:56 +0800

    chore: add notice

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,21 @@
+import { Alert } from 'antd'
 import list from '../utils/router'
 export default function index() {
 
   return (
     <div className='max-w-xl mx-auto mt-2'>
+      <Alert
+        message="公告"
+        closable
+        description={<>
+          <div>BUG反馈/功能建议/摸鱼划水 欢迎加入QQ群: <b>752693422</b></div>
+          <div>因为网站完全由一个18岁苦逼学生制作，更新可能较慢，更多的模板正在制作中喔，有更好的需求或建议也欢迎联系我！</div>
+          <div>因为网站运营需要一定成本,如果您手中富裕不妨 <a href="/sponsor">支持一下</a> 网站运营！</div>
+          <div>或者在 <a href="https://github.com/YuzeTT/small-sticker">Github</a> 给我个Star 对我的帮助也非常大！！</div>
+        </>}
+        type="info"
+        className='mb-2 mt-4'
+      />
       <div text='sm' op50 mb-2>已完成</div>
       <div className='grid grid-cols-1 md:grid-cols-2 gap-2'>
         {list.done.map((item, key)=>(
---
+++ src/pages/sponsor.tsx
@@ -0,0 +1,59 @@
+import { Collapse, Table } from 'antd';
+import type { ColumnsType } from 'antd/es/table';
+import Line from '../components/Line';
+
+interface DataType {
+  key: React.Key;
+  name: string;
+  money: number;
+  remark: string;
+}
+
+const columns: ColumnsType<DataType> = [
+  {
+    title: '昵称',
+    dataIndex: 'name',
+    // render: (text: string) => <a>{text}</a>,
+  },
+  {
+    title: '金额',
+    dataIndex: 'money',
+    render: (text: number) => <span>￥{text.toFixed(2)}</span>,
+  },
+  {
+    title: '留言',
+    dataIndex: 'remark',
+  },
+];
+
+const data: DataType[] = [
+  {
+    key: 'Ric',
+    name: 'John Brown',
+    money: 2.88,
+    remark: '网站很好用哇，谢谢！',
+  },
+];
+
+export default function sponsor() {
+  return (
+    <div className=''>
+      <div my-4>网站的维护需要一定运营成本,如果能收到您的赞助,我会非常开心！！！</div>
+      <Collapse className='mb-4'
+        items={[{ key: '1', label: '点击展开收款码', children: <div>
+          <div>付款后联系微信: hong_yu1024 可以自定义昵称和留言哦!（24小时没回复就帮你登记付款自带的啦~）</div>
+          <div>也可以在付款备注留言和你的名字哦!</div>
+          <div>最后谢谢你! mua~</div>
+          <img src="/wechat.JPG" alt="" w-full />
+        </div> }]}
+      />
+      <Line zh='赞助列表' en='Sponsor List' logo={<div className="i-ri-file-list-2-line" mr-4 text='xl' />}></Line>
+      <div className='-mx-4 -mt-2'>
+        <Table
+          columns={columns}
+          dataSource={data}
+        />
+      </div>
+    </div>
+  )
+}
