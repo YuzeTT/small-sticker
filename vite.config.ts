7e2f88f81acfb9c334bd6029a962598f98a77d37
YuzeTT <ziv1024@163.com>
2023-10-18 09:45:00 +0800

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import UnoCSS from 'unocss/vite'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    UnoCSS(),
+  ],
+})
