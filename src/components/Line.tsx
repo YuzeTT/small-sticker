4d6386c80fbba16e1f969610e7e3e03d096fb238
YuzeTT <ziv1024@163.com>
2023-10-18 16:32:12 +0800

    fix: 'children' is missing in type

--- src/components/Line.tsx
+++ src/components/Line.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from "react";
 
-export default function Line({zh, en, logo, children}:{zh: string, en:string, logo:ReactNode, children:ReactNode}) {
+export default function Line({zh, en, logo, children}:{zh: string, en:string, logo:ReactNode, children?:ReactNode}) {
   return (
     <div className="-mx-4 px-4 bg-zinc-50 py-2 mb-2" flex='~ items-center'>
       {logo}
