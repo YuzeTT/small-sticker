e12bc7763c04e71988bc1b159ff60d115ebffdc6
onepisYa <pis1@qq.com>
2023-10-19 13:59:35 +0800

    chore: 🔧 alias src to @

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import Pages from 'vite-plugin-pages'
 import UnoCSS from 'unocss/vite'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'node:path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,4 +11,9 @@ export default defineConfig({
     react(),
     UnoCSS(),
   ],
+  resolve:{
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  }
 })
