2c215e221b3972d3a8220a595170a6f3491f5a37
YuzeTT <ziv1024@163.com>
2023-10-19 18:42:28 +0800

    chore: update train

--- src/pages/train.tsx
+++ src/pages/train.tsx
@@ -1,4 +1,5 @@
-import { AutoComplete, Button, Modal, DatePicker, Input, Space, message, Segmented } from "antd";
+import { AutoComplete, QRCode, Button, Modal, DatePicker, Input, Space, message, Segmented } from "antd";
+import { pinyin } from 'pinyin-pro';
 import { useCallback, useRef, useState } from "react";
 import downloadHtmlAsImage from "../utils/downloadHtmlAsImage";
 
@@ -23,6 +24,7 @@ export default function Train() {
     sit2: '', // 号
     sit3: '', // 座位
     isShowLogo: 0, // 水印
+    qrcode: 'https://sticker.hsott.cn', // 二维码
   })
   const ref = useRef<HTMLDivElement>(null)
   const [messageApi, contextHolder] = message.useMessage();
@@ -113,7 +115,7 @@ export default function Train() {
   const fillTest = () => {
     setData({
       name: '马斯克', // 名字
-      id: '888888888888888888', // 身份证
+      id: '123456789123456789', // 身份证
       station1: '上海', // 始发站
       station2: '西安', // 到达站
       ticketCode: 'T000001', // 左上角票码
@@ -128,6 +130,7 @@ export default function Train() {
       sit2: '001', // 号
       sit3: 'F', // 座位
       isShowLogo: 0, // 水印
+      qrcode: 'https://sticker.hsott.cn', // 二维码
     })
   }
 
@@ -154,66 +157,44 @@ export default function Train() {
           <Button onClick={fillTest}>测试数据</Button>
         </Line>
         <Space direction="vertical" className="w-full">
+          <div flex='~ justify-between gap-2'>
+            <Input placeholder="左上角票号" value={data.ticketCode} onChange={(v)=>setData({...data, ticketCode: v.target.value})}/>
+            <Input placeholder="检票口" addonBefore='检票' value={data.checkCode} onChange={(v)=>setData({...data, checkCode: v.target.value})}/>
+          </div>
           <div flex='~ items-center justify-between gap-2'>
             <Input className="w-50" placeholder="始发站" addonAfter='站' value={data.station1} onChange={(v)=>setData({...data, station1: v.target.value})}/>
             <Input className="w-50" placeholder="车次" addonBefore='>' addonAfter='>' value={data.railwayCode} onChange={(v)=>setData({...data, railwayCode: v.target.value})}/>
             <Input className="w-50" placeholder="到达站" addonAfter='站' value={data.station2} onChange={(v)=>setData({...data, station1: v.target.value})}/>
           </div>
           <Space.Compact className="w-full">
-            <DatePicker placeholder="日期" onChange={(_,v)=>setData({...data, date: v})} format={'YYYY-MM-DD'} showToday />
+            <DatePicker placeholder="日期" onChange={(_,v)=>setData({...data, date: v})} format={'YYYY 年 MM 月 DD 日'} showToday />
             <DatePicker picker="time" placeholder="时间" onChange={(_,v)=>setData({...data, time: v})} format={'HH:mm'} use12Hours={false}  />
           </Space.Compact>
-{/* 
-          <Space.Compact>
-            <Input placeholder="取餐码" value={data.code} onChange={(v)=>setData({...data, code: v.target.value})}/>
-            <AutoComplete
-              options={[{value: '自提'},{value: '堂食'}]}
-              className="w-30"
-              value={data.type} onChange={(v)=>setData({...data, type: v})}
-              placeholder="取餐方式"
-            />
-            
-          </Space.Compact>
-          <Space>
-            <Input addonBefore="第" placeholder="1" className="" value={data.number1} onChange={(v)=>setData({...data, number1: v.target.value})}/>
-            <span className="">/</span>
-            <Input addonAfter="杯" placeholder="1" className="" value={data.number2} onChange={(v)=>setData({...data, number2: v.target.value})}/>
-          </Space>
           <Space.Compact className="w-full">
+            <Input placeholder="车次" value={data.sit1} onChange={(v)=>setData({...data, sit1: v.target.value})} addonAfter='车'/>
+            <Input placeholder="号次" value={data.sit2} onChange={(v)=>setData({...data, sit2: v.target.value})} addonAfter='号'/>
+            <Input placeholder="座位" value={data.sit3} onChange={(v)=>setData({...data, sit3: v.target.value})} />
+          </Space.Compact>
+          <Space.Compact className="w-full">
+            <Input placeholder="价格" value={data.price} onChange={(v)=>setData({...data, price: v.target.value})} addonAfter='元'/>
             <AutoComplete
-              options={[{value: '【热】'},{value: '【冰】'}]}
-              className="w-20"
-              value={data.temp} onChange={(v)=>setData({...data, temp: v})}
-              placeholder="温度"
-            />
-            <AutoComplete
-              options={[{value: '马斯卡彭生酪拿铁'},{value: '生椰拿铁'}]}
-              className="flex-1"
-              value={data.product} onChange={(v)=>setData({...data, product: v})}
-              placeholder="商品"
-            />
-            <AutoComplete
-              options={[{value: '标准糖'},{value: '半糖'},{value: '不另外加糖'}]}
-              className=" w-30"
-              placeholder="甜度"
-              value={data.sweet} onChange={(v)=>setData({...data, sweet: v})}
+              options={[{value: '二等座'},{value: '一等座'},{value: '商务座'},{value: '软卧'}]}
+              className="w-30"
+              placeholder="座位类型"
+              value={data.type} onChange={(v)=>setData({...data, type: v})}
             />
           </Space.Compact>
-          
-          <Input placeholder="备注" defaultValue={'建议尽快饮用，风味更佳'} className="" value={data.remark} onChange={(v)=>setData({...data, remark: v.target.value})}/> */}
+          <Space.Compact className="w-full">
+            <Input placeholder="身份证号码" value={data.id} onChange={(v)=>setData({...data, id: v.target.value})} className='w-[200%]'/>
+            <Input placeholder="姓名" value={data.name} onChange={(v)=>setData({...data, name: v.target.value})} />
+          </Space.Compact>
+          <Input placeholder="底部信息" value={data.uniCode} onChange={(v)=>setData({...data, uniCode: v.target.value})}/>
+          <Input placeholder="二维码链接" addonBefore='二维码' value={data.qrcode} onChange={(v)=>setData({...data, qrcode: v.target.value})} disabled/>
           <Segmented options={[{label:'隐藏Logo', value:0}, {label:'显示Logo', value:1}]} value={data.isShowLogo} onChange={(v)=>{
             if(v===1){
               Modal.confirm({
                 title: '免责声明',
                 content: '加入Logo是您的个人行为，与本站无关，所造成的后果自行承担。',
-                // footer: (_, { OkBtn, }) => (
-                //   <>
-                //     <Button onClick={()=>{
-                //       setData({...data, isShowLogo: 0})
-                //     }}>不同意</Button>
-                //     <OkBtn />
-                //   </>
-                // ),
                 okText: '我同意',
                 cancelText: '我拒绝',
                 onOk() {
@@ -231,7 +212,7 @@ export default function Train() {
         <div my-2></div>
         <Line zh='预览' en='Preview' logo={<div className="i-ri-landscape-line" mr-4 text='xl' />}></Line>
         <div className="flex items-center justify-center">
-          <div className="bg-blue-100 rounded-md overflow-hidden relative w-100 " ref={ref}>
+          <div className="bg-blue-100 rounded-md overflow-hidden relative min-w-100 scale-75 md:scale-100 relative" ref={ref}>
             <div flex='~ justify-between'>
               <div text='xl red-500' ml-4 mt-1>{data.ticketCode}</div>
               <div text='' mr-8 mt-1>检票:{data.checkCode}</div>
@@ -242,7 +223,7 @@ export default function Train() {
                   <span text='2xl' className="tracking-[1rem]">{data.station1}</span>
                   <span text='lg'>站</span>
                 </div>
-                <div text='center' className='-mt-1'>Pinyin</div>
+                <div text='center' className='-mt-1'>{pinyin(data.station1, { toneType: 'none' }).charAt(0).toUpperCase()}{pinyin(data.station1, { toneType: 'none' }).slice(1)}</div>
               </div>
               <div>
                 <div text='xl center' mr-2 className='-mb-4 tracking-widest'>{data.railwayCode}</div>
@@ -253,7 +234,7 @@ export default function Train() {
                   <span text='2xl' className="tracking-[1rem]">{data.station2}</span>
                   <span text='lg'>站</span>
                 </div>
-                <div text='center' className='-mt-1'>Pinyin</div>
+                <div text='center' className='-mt-1'>{pinyin(data.station2, { toneType: 'none' }).charAt(0).toUpperCase()}{pinyin(data.station2, { toneType: 'none' }).slice(1)}</div>
               </div>
             </div>
             <div flex='~ justify-between'>
@@ -267,7 +248,7 @@ export default function Train() {
               </div>
             </div>
             <div mt-4 ml-4>仅供收藏使用</div>
-            <div ml-4>{data.id} {data.name}</div>
+            <div ml-4>{data.id.slice(0, 10)}****{data.id.slice(14, 18)} {data.name}</div>
 
             <div text='center sm' ml-8 px-10 inline-block mt-1 style={{border: '1.5px dashed'}}>
               <div>报销凭证 遗失不补</div>
@@ -277,20 +258,8 @@ export default function Train() {
             <div className='bg-blue-300' mt-1 px-4 py-1>
               <div>{data.uniCode}</div>
             </div>
-            {/* {data.isShowLogo? <img src="/lucking_coffee_dark.svg" alt="" className="absolute w-8 right-4 top-5" />:''} */}
-            {/* <div className="px-3 text-xl">Hi，{data.name || '未填写'} {data.sex}</div>
-            <div className="flex items-center px-3 pt-2">
-              <div className="font-bold text-4xl">{data.code || '000'}</div>
-              <div className="text-lg pl-2">{data.type || '自己飞走'}</div>
-              <div className="text-lg pl-2">第{data.number1 || '1'}/{data.number2 || '1'}杯</div>
-            </div>
-            <div className="flex">
-              <div className="text-lg pl-2">{data.temp || '【烫烫烫】'}</div>
-              <div className="text-lg pl-2">{data.product || '锟斤拷'}</div>
-            </div>
-            <div className="text-lg pl-2">{data.sweet || '胰岛素配合'}</div>
-            <div className="pl-2 font-bold pt-4 line-height-2">{data.date || '0000-00-00'} {data.time || '00:00'}</div>
-            <div className="pl-2 pb-3">{data.remark}</div> */}
+
+            <QRCode value={data.qrcode || 'https://sticker.hsott.cn'} className='absolute right-1 bottom-7' size={100} bordered={false} />
           </div>
         </div>
 
