a78aaf1c940c71c9842d76c38169151a14227672
YuzeTT <ziv1024@163.com>
2023-10-20 16:39:27 +0800

    chore: add warning text

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,12 +6,19 @@ export default function Footer() {
       <div className='text-sm op-50' onClick={()=>{
         Modal.info({
           title: '调试信息',
+          okText: '上传问题',
+          closable: true,
           content: (
             <div>
               <p>版本：v0.2.60</p>
               <p>时间：{new Date().toLocaleString()}</p>
               <p>ID：{localStorage.getItem("id")}</p>
               <p>决断ID：{localStorage.getItem("userId")}</p>
+              <p>上传后请将 决断ID 和 时间 反馈给开发者</p>
+              <div text='sm red-500' className="-mt-1" flex='~ items-center'>
+                <div className="i-ri-error-warning-line mr-1" />
+                <div>您的操作过程将被上传，请注意隐私保护！</div>
+              </div>
             </div>
           )
         });
