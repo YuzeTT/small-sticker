b2c86971411e42ef6c684483b6e66513fa0b9305
YuzeTT <ziv1024@163.com>
2023-10-21 21:30:01 +0800

    chore: update maoyan

--- src/components/HighText.tsx
+++ src/components/HighText.tsx
@@ -1,12 +1,23 @@
-export default function HighText({text, show}:{text: string, show: boolean}) {
+export default function HighText({text, show, eg}:{text?: string, show: boolean, eg?: string}) {
   return (
-    <div inline-block align-top mb-1>
+    <div inline-block align-top mb-1 className='rounded-1 overflow-hidden'>
       <div contentEditable={true} style={show?{padding: '0 0.25rem', color: '#000000', background: '#DBEAFE', border: '1.5px solid #3B82F6', borderRadius: '0.25rem 0.25rem 0 0'}:{}} outline-none></div>
       {show?
-        <div text='xs white' bg='blue-500' px-1 flex='~ items-center' className='py-0.5 rounded-b-1'>
-          <div className="i-ri-corner-left-up-line" />
-          {/* <div className="i-ri-arrow-up-line" /> */}
-          <div>{text}</div>
+        <div>
+          <div text='xs white' bg='blue-500' px-1 flex='~ items-center' className='py-0.5'>
+            <div className="i-ri-corner-left-up-line" />
+            {/* <div className="i-ri-arrow-up-line" /> */}
+            <div>{text}</div>
+          </div>
+          {eg?
+            <div text='xs white' bg='teal-500' px-1 flex='~ items-center' className='py-0.5'>
+              {/* <div className="i-ri-corner-left-up-line" /> */}
+              {/* <div className="i-ri-arrow-up-line" /> */}
+              <div className="i-ri-expand-right-line" />
+              <div>{eg}</div>
+            </div>
+            :''
+          }
         </div>
         :''
       }
--- src/pages/maoyan.tsx
+++ src/pages/maoyan.tsx
@@ -16,25 +16,46 @@ export default function Maoyan() {
             {/* left */}
             <div className="w-[75%]">
               <img src="/maoyan-2.png" alt="logo" h-7 pt-3 pb-1 />
-              <div text='sm'><HighText show={highLight} text='影院名称' />（<HighText show={highLight} text='店铺地址'></HighText>）</div>
-              <div contentEditable text='sm'>电影名称(2D/3D)</div>
+              <div text='sm'><HighText show={highLight} text='影院名称' eg='万达影城' /></div>
+              <div text='sm'>
+                <HighText show={highLight} text='电影名称' eg='深海(3D)' />
+                {/* (<HighText show={highLight} text='电影形式' eg='3D' />) */}
+              </div>
               <div mt-1 flex='~ items-center justify-between'>
-                <div contentEditable text='sm'>影厅特色</div>
-                <div contentEditable>观影时间</div>
+                <div text='sm'>
+                  <HighText show={highLight} text='影厅' eg='9号VIP厅' />
+                </div>
+                <div>
+                  <HighText show={highLight} text='开始时间' eg='19:00' />
+                </div>
               </div>
               <div flex='~ items-center justify-between'>
-                <div contentEditable>1排3号</div>
-                <div contentEditable>观影日期</div>
+                <div>
+                  <HighText show={highLight} text='座位' eg='5排3号' />
+                </div>
+                <div>
+                  <HighText show={highLight} text='开始日期' eg='2023-08-11' />
+                </div>
               </div>
               <div flex='~' mt-1>
                 <QRCode value={'https://sticker.hsott.cn/qrcode?text=qwqqwq'} bordered={false} className="-m-3" size={140} />
                 <div ml-2>
-                  <div contentEditable text='xs'>票价：35.0元</div>
-                  <div contentEditable text='xs'>服务费：10.0元</div>
-                  <div contentEditable text='xs'>08-11 18:47</div>
-                  <div contentEditable text='xs' mt-3>网络售票</div>
-                  <div contentEditable text='xs'>12150000</div>
-                  <div contentEditable text='xs'>41004314</div>
+                  <div text='xs'>票价：<HighText show={highLight} text='' />元</div>
+                  <div text='xs'>服务费：<HighText show={highLight} text=''/>元</div>
+                  <div text='xs'>
+                    <HighText show={highLight} text='日期' eg='08-11' />
+                    {' '}
+                    <HighText show={highLight} text='时间' eg='15:24' />
+                  </div>
+                  <div text='xs' mt-3>
+                    <HighText show={highLight} text='售票渠道' eg='网络售票' />
+                  </div>
+                  <div text='xs'>
+                    <HighText show={highLight} text='机器码1' eg='13150000' />
+                  </div>
+                  <div text='xs'>
+                    <HighText show={highLight} text='机器码2' eg='03352952' />
+                  </div>
                 </div>
               </div>
             </div>
@@ -43,14 +64,30 @@ export default function Maoyan() {
               <div flex='~ justify-center'>
                 <div px-2 className='py-0.5' rounded-full bg-red-500 text='white'>副券</div>
               </div>
-              <div contentEditable text='xs' mt-2>影厅</div>
-              <div contentEditable text='xs'>座位</div>
-              <div contentEditable text='xs' mt-4>时间</div>
-              <div contentEditable text='xs'>日期</div>
-              <div contentEditable text='xs' mt-4>电影名称</div>
-              <div contentEditable text='xs' mt-4>票价</div>
-              <div contentEditable text='xs' mt-4>编号1</div>
-              <div contentEditable text='xs'>编号2</div>
+              <div text='xs' mt-2>
+                <HighText show={highLight} text='影厅' eg='9号VIP厅' />
+              </div>
+              <div text='xs'>
+                <HighText show={highLight} text='座位' eg='5排3号' />
+              </div>
+              <div text='xs' mt-4>
+                <HighText show={highLight} text='时间' eg='15:24' />
+              </div>
+              <div text='xs'>
+                <HighText show={highLight} text='日期' eg='08-11' />
+              </div>
+              <div text='xs' mt-4>
+                <HighText show={highLight} text='电影名称' eg='深海(3D)' />
+              </div>
+              <div text='xs' mt-4>
+                <HighText show={highLight} text='票价' />元
+              </div>
+              <div text='xs' mt-4>
+                <HighText show={highLight} text='机器码1' eg='13150000' />
+              </div>
+              <div text='xs'>
+                <HighText show={highLight} text='机器码2' eg='03352952' />
+              </div>
             </div>
           </div>
           <div px-4 py-2 bg='red-500' mt-4 flex='~ items-center justify-between'>
