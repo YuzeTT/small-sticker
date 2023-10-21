0c6b83ddb1e5e5d9759302f98b03796b31839e3f
YuzeTT <ziv1024@163.com>
2023-10-21 23:00:58 +0800

    chore: maoyan is done

--- src/pages/maoyan.tsx
+++ src/pages/maoyan.tsx
@@ -2,6 +2,7 @@ import { Alert, QRCode, Segmented, message, Button, Empty, Image } from "antd";
 import { useCallback, useRef, useState } from "react";
 import HighText from "../components/HighText";
 import showImage from "../utils/downloadHtmlAsImage/showImage";
+import SecureWatermark from "../components/SecureWatermark";
 
 export default function Maoyan() {
   const ref = useRef<HTMLDivElement>(null)
@@ -77,89 +78,91 @@ export default function Maoyan() {
       </div>
       <div mt-4 p-2>
         <div className='flex justify-center'>
-          <div bg='white' className='w-80 shadow-xl' ref={ref} style={status===2?{display: 'none'}:{}}>
-            <div pl-4 flex='~ justify-between'>
-              {/* left */}
-              <div className="w-[75%]">
-                <img src="/maoyan-2.png" alt="logo" h-7 pt-3 pb-1 />
-                <div text='sm'><HighText show={highLight} text='影院名称' eg='万达国际影城（福州台江店）' /></div>
-                <div text='sm'>
-                  <HighText show={highLight} text='电影名称' eg='深海(3D)' />
-                  {/* (<HighText show={highLight} text='电影形式' eg='3D' />) */}
-                </div>
-                <div mt-1 flex='~ items-center justify-between'>
+          <div bg='white' className='w-80 shadow-xl relative' ref={ref} style={status===2?{display: 'none'}:{}}>
+            <SecureWatermark>
+              <div pl-4 flex='~ justify-between'>
+                {/* left */}
+                <div className="w-[75%]">
+                  <img src="/maoyan-2.png" alt="logo" h-7 pt-3 pb-1 />
+                  <div text='sm'><HighText show={highLight} text='影院名称' eg='万达国际影城（福州台江店）' /></div>
                   <div text='sm'>
-                    <HighText show={highLight} text='影厅' eg='1号VIP厅' />
+                    <HighText show={highLight} text='电影名称' eg='深海(3D)' />
+                    {/* (<HighText show={highLight} text='电影形式' eg='3D' />) */}
                   </div>
-                  <div>
-                    <HighText show={highLight} text='开始时间' eg='19:00' />
+                  <div mt-1 flex='~ items-center justify-between'>
+                    <div text='sm'>
+                      <HighText show={highLight} text='影厅' eg='1号VIP厅' />
+                    </div>
+                    <div>
+                      <HighText show={highLight} text='开始时间' eg='19:00' />
+                    </div>
+                  </div>
+                  <div flex='~ items-center justify-between'>
+                    <div>
+                      <HighText show={highLight} text='座位' eg='5排3号' />
+                    </div>
+                    <div>
+                      <HighText show={highLight} text='开始日期' eg='2023-08-11' />
+                    </div>
+                  </div>
+                  <div flex='~' mt-1>
+                    <QRCode value={'https://sticker.hsott.cn/qrcode?text=qwqqwq'} bordered={false} className="-m-3" size={140} />
+                    <div ml-2>
+                      <div text='xs'>票价：<HighText show={highLight} text='' eg='10' />元</div>
+                      <div text='xs'>服务费：<HighText show={highLight} text='' eg='10'/>元</div>
+                      <div text='xs'>
+                        <HighText show={highLight} text='日期' eg='08-11' />
+                        {' '}
+                        <HighText show={highLight} text='时间' eg='15:24' />
+                      </div>
+                      <div text='xs' mt-3>
+                        <HighText show={highLight} text='售票渠道' eg='网络售票' />
+                      </div>
+                      <div text='xs'>
+                        <HighText show={highLight} text='机器码1' eg='13150000' />
+                      </div>
+                      <div text='xs'>
+                        <HighText show={highLight} text='机器码2' eg='03352952' />
+                      </div>
+                    </div>
                   </div>
                 </div>
-                <div flex='~ items-center justify-between'>
-                  <div>
+                {/* right */}
+                <div py-3 ml-4 className="w-[25%]">
+                  <div flex='~ justify-center'>
+                    <div px-2 className='py-0.5' rounded-full bg-red-500 text='white'>副券</div>
+                  </div>
+                  <div text='xs' mt-2>
+                    <HighText show={highLight} text='影厅' eg='9号VIP厅' />
+                  </div>
+                  <div text='xs'>
                     <HighText show={highLight} text='座位' eg='5排3号' />
                   </div>
-                  <div>
-                    <HighText show={highLight} text='开始日期' eg='2023-08-11' />
+                  <div text='xs' mt-4>
+                    <HighText show={highLight} text='时间' eg='15:24' />
                   </div>
-                </div>
-                <div flex='~' mt-1>
-                  <QRCode value={'https://sticker.hsott.cn/qrcode?text=qwqqwq'} bordered={false} className="-m-3" size={140} />
-                  <div ml-2>
-                    <div text='xs'>票价：<HighText show={highLight} text='' eg='10' />元</div>
-                    <div text='xs'>服务费：<HighText show={highLight} text='' eg='10'/>元</div>
-                    <div text='xs'>
-                      <HighText show={highLight} text='日期' eg='08-11' />
-                      {' '}
-                      <HighText show={highLight} text='时间' eg='15:24' />
-                    </div>
-                    <div text='xs' mt-3>
-                      <HighText show={highLight} text='售票渠道' eg='网络售票' />
-                    </div>
-                    <div text='xs'>
-                      <HighText show={highLight} text='机器码1' eg='13150000' />
-                    </div>
-                    <div text='xs'>
-                      <HighText show={highLight} text='机器码2' eg='03352952' />
-                    </div>
+                  <div text='xs'>
+                    <HighText show={highLight} text='日期' eg='08-11' />
+                  </div>
+                  <div text='xs' mt-4>
+                    <HighText show={highLight} text='电影名称' eg='深海(3D)' />
+                  </div>
+                  <div text='xs' mt-4>
+                    <HighText show={highLight} text='票价' eg='20' />元
+                  </div>
+                  <div text='xs' mt-4>
+                    <HighText show={highLight} text='机器码1' eg='13150000' />
+                  </div>
+                  <div text='xs'>
+                    <HighText show={highLight} text='机器码2' eg='03352952' />
                   </div>
                 </div>
               </div>
-              {/* right */}
-              <div py-3 ml-4 className="w-[25%]">
-                <div flex='~ justify-center'>
-                  <div px-2 className='py-0.5' rounded-full bg-red-500 text='white'>副券</div>
-                </div>
-                <div text='xs' mt-2>
-                  <HighText show={highLight} text='影厅' eg='9号VIP厅' />
-                </div>
-                <div text='xs'>
-                  <HighText show={highLight} text='座位' eg='5排3号' />
-                </div>
-                <div text='xs' mt-4>
-                  <HighText show={highLight} text='时间' eg='15:24' />
-                </div>
-                <div text='xs'>
-                  <HighText show={highLight} text='日期' eg='08-11' />
-                </div>
-                <div text='xs' mt-4>
-                  <HighText show={highLight} text='电影名称' eg='深海(3D)' />
-                </div>
-                <div text='xs' mt-4>
-                  <HighText show={highLight} text='票价' eg='20' />元
-                </div>
-                <div text='xs' mt-4>
-                  <HighText show={highLight} text='机器码1' eg='13150000' />
-                </div>
-                <div text='xs'>
-                  <HighText show={highLight} text='机器码2' eg='03352952' />
-                </div>
+              <div px-4 py-2 bg='red-500' mt-4 flex='~ items-center justify-between'>
+                <div text='white xs'>凭此影票 入场观看</div>
+                <img src="/maoyan-3.png" alt="logo" h-4 />
               </div>
-            </div>
-            <div px-4 py-2 bg='red-500' mt-4 flex='~ items-center justify-between'>
-              <div text='white xs'>凭此影票 入场观看</div>
-              <img src="/maoyan-3.png" alt="logo" h-4 />
-            </div>
+            </SecureWatermark>
           </div>
         </div>
         {status===1?
