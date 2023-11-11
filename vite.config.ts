8fce5b72c4f56210be6afe698a2e025d52b7cf18
expload <38076731+expload233@users.noreply.github.com>
2023-11-11 08:55:53 +0000

    Use PWA

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import Pages from 'vite-plugin-pages'
 import UnoCSS from 'unocss/vite'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa'
 import path from 'node:path';
 
 // https://vitejs.dev/config/
@@ -10,6 +11,32 @@ export default defineConfig({
     Pages(),
     react(),
     UnoCSS(),
+    VitePWA({ registerType: 'autoUpdate',  
+    includeAssets: ['sticker_logo.svg'],
+    manifest: {
+      name: '大贴纸',
+      short_name: '大贴纸',
+      theme_color: '#ffffff',
+      icons: [
+        {
+          src: 'pwa-192x192.png', 
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: '/pwa-512x512.png', 
+          sizes: '512x512',
+          type: 'image/png',
+        },
+        {
+          src: 'pwa-512x512.png', 
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any maskable',
+        },
+      ],
+    },
+  }),
   ],
   resolve:{
     alias: {
