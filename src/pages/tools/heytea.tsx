cbaca224baaf120b9df5b67ab4fc310435ce3676
YuzeTT <ziv1024@163.com>
2023-11-11 19:35:59 +0800

    fix: heytea word wrap

--- src/pages/tools/heytea.tsx
+++ src/pages/tools/heytea.tsx
@@ -11,7 +11,7 @@ export default function Heytea() {
   return (
     <div>
       <BaseCard ref={ref}>
-        <div bg='orange-100' className='w-65 z-0 relative px-6 py-4'>
+        <div bg='orange-100' className='w-70 z-0 relative px-6 py-4'>
           <img src="/images/heytea.png" alt="logo" absolute w-10 h-auto right-4 top-4 op80 />
           <div flex='~ items-center'>
             {/* <div text='xs' px-1 style={{border: '1px solid'}}></div> */}
@@ -34,14 +34,14 @@ export default function Heytea() {
           </div>
           <div flex='~ justify-between'>
             <div flex='~'>
-              <div text='sm' font='bold' className='mt-6'>
+              <div text='sm' font='bold' className='mt-6 flex-shrink-0'>
                 <HighText show={highLight} text='产品介绍' eg='风味基因' />
               </div>
-              <div text='sm' className='mt-6 ml-2'>
-                <HighText show={highLight} text='产品介绍' eg='清爽茗茶' />
+              <div text='sm' className='mt-6 ml-2 flex-1'>
+                <HighText show={highLight} text='介绍(手动换行)' eg='清爽茗茶' />
               </div>
             </div>
-            <div text='sm' className='mt-6 text-right'>
+            <div text='sm' className='mt-6 text-right flex-shrink-0'>
               <HighText show={highLight} text='小标签' eg='真牛乳' />
             </div>
           </div>
