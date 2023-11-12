f28aed8fe6b9ac29fef412379ab959a5cd2eed5d
YuzeTT <ziv1024@163.com>
2023-11-13 00:52:05 +0800

    chore: update router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,14 @@
 import { Modal, message } from 'antd'
 // import { Button } from '@chakra-ui/react'
+import { useNavigate } from 'react-router-dom';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import list from '../utils/router'
 import FollowMe from '../components/FollowMe';
 import isVip from '../utils/isVip';
 
 export default function index() {
+  const navigate = useNavigate();
+
   const jumpBefore = (url: string) => {
     if(localStorage.getItem("isFollowMe")!=='true'){
       Modal.info({
@@ -29,23 +32,10 @@ export default function index() {
   }
   
   const jump = (url: string) => {
-    window.open(url)
+    navigate(url)
   }
   return (
     <div className='max-w-xl mx-auto'>
-      {/* <Button colorScheme='blue'>Button</Button> */}
-      {/* <img src="/bei.png" alt="" rounded-xl w-full mb-2 /> */}
-      {/* <div p-4 bg='white' rounded-xl mb-4>
-        <div text='xl zinc-800' font='bold'>公告</div>
-        <div text='sm zinc-600' mt-3>BUG反馈/功能建议/摸鱼划水 QQ群: 752693422</div>
-        <div text='sm zinc-600'>
-          因为网站运营需要一定成本，如果您手中富裕不妨 <a href="/sponsor" decoration-none text-blue-500>支持一下</a> 网站运营！
-        </div>
-        <div text='sm zinc-600'>
-          或者在 <a href="https://github.com/YuzeTT/small-sticker"  decoration-none text-blue-500>Github</a> 给我个Star 对我的帮助也非常大！！ 你可以 <a href="/hero"  decoration-none text-blue-500>在这里</a> 查看伟大的贡献者们！
-        </div>
-        <div text='sm orange-600' mt-2><b>安卓端夸克</b>暂时无法保存图片（iOS端夸克不受影响）另外如果你有办法解决欢迎联系（有赏）</div>
-      </div> */}
       <div className='p-2 rounded-2xl mb-4 card'>
         <div className='mb-3 mt-1 mx-2 text-zinc-600 text-sm'>快捷跳转</div>
         <div className='grid grid-cols-2 gap-2'>
