584d6161d19d0b5664a2863d743a3d93701c7c50
YuzeTT <ziv1024@163.com>
2023-11-30 00:02:54 +0800

    chore: upd

--- src/pages/notice.tsx
+++ src/pages/notice.tsx
@@ -71,7 +71,7 @@ export default function Notice() {
             <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
             <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">2023/11/28</time>
             <h3 className="text-lg font-semibold text-gray-900 dark:text-white">网站资金源切断</h3>
-            <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">小红书回收了所有权限，网站至此也再无流量。网站采取保守措施，保留所有功能但是切换价格较低的服务商，网站暂停更新。</p>
+            <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">小红书回收了所有权限，网站至此也再无流量。网站采取保守措施，保留所有功能但是完全依靠赞助生存。</p>
           </li>
           <li className="mb-10 ms-4">
             <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
